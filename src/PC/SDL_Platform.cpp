e6e46ef12e8f23ab810516ce53e666483d248cc2
Sam Serrels <doogly1@gmail.com>
2015-02-09 10:43:13 +0000

    OGL KHR warning filter.

--- src/PC/SDL_Platform.cpp
+++ src/PC/SDL_Platform.cpp
@@ -71,7 +71,9 @@ void __stdcall printOutKhrDebugMessage(GLenum source, GLenum type, GLuint id,
   GLenum severity, GLsizei length,
   const GLchar* message,
   const void* userParam) {
-  printf("\nAn OGL KHR error has occured: %s\n", message);
+	if (severity >= GL_DEBUG_SEVERITY_LOW){
+	printf("\nAn OGL KHR error has occured: %s\n", message);
+	}
 }
 
 namespace Engine {
